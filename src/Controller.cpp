d3a346f005441316b8b1ec2c49dbd18f3381ad66
Theo Stavrou <theostavrou21@gmail.com>
2026-03-02 18:51:25 +0000

    Add anti-windup via conditional integral integration

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -14,7 +14,16 @@ double Controller::computeThrottle(double target_velocity,
 {
     double error = target_velocity - current_velocity;
 
-    integral_error_ += error * dt;
+    double proposed_integral = integral_error_ + error * dt;
+
+    double unclamped_throttle = kp_ * error + ki_ * proposed_integral;
+
+    bool saturating_high = unclamped_throttle > 1.0 && error > 0.0;
+    bool saturating_low = unclamped_throttle < 0.0 && error < 0.0;
+
+    if(!saturating_high && !saturating_low) {
+        integral_error_ = proposed_integral;
+    }
 
     double throttle = kp_ * error + ki_ * integral_error_;
 
